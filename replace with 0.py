a5f4aaa50c769ef0a12a6e1b831999d316925667
HanishaP24 <70179504+HanishaP24@users.noreply.github.com>
2020-09-24 20:24:00 -0400

    Add files via upload

---
+++ replace with 0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import pandas as pd
+df = pd.read_csv("C:/Users/Hanisha/Downloads/gradedata2.csv")
+df.head()
+df.fillna(0)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
