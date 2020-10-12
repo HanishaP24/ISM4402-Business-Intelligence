efc2267835acd2133845ca0f8e36eef61e19697f
HanishaP24 <70179504+HanishaP24@users.noreply.github.com>
2020-10-11 22:11:25 -0400

    Add files via upload

---
+++ DataFrame.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+names = ['Bob','Jessica','Mary','John','Mel']
+grades = [76,95,77,78,79]
+bsdegrees = [1,1,0,0,1]
+msdegrees = [2,1,0,0,0]
+phdegrees = [0,1,0,0,0]
+
+
+# In[ ]:
+
+
+
+
